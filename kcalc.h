dd2ab4f73f8ad5678356a8157746491227a6f7ff
Bernd Wuebben <wuebben@kde.org>
1997-08-03 02:06:15 +0000

    Bernd -- Clean ups
    svn path=/trunk/kdeutils/kcalc/; revision=636

--- kcalc.h
+++ kcalc.h
@@ -61,7 +61,6 @@
 // and this for all the poor devels out there who don't have long double math
 // I guess it's time to switch to LINUX guys .....
 
-// #define HAVE_FABSL
 /* TAKE CARE OF TH HAVE_LONG_DOUBLE defines in core.cpp*/
 
 
