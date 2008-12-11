0fd97820f3f819b42ff135fec9cc7bf45babff60
David Johnson <david@usermode.org>
2008-12-11 07:45:52 +0000

    BUG: 148414 Convert pasted string to lowercase before processing. Fixes e vs E.
    svn path=/trunk/KDE/kdeutils/kcalc/; revision=895607

--- kcalcdisplay.cpp
+++ kcalcdisplay.cpp
@@ -250,11 +250,12 @@ void KCalcDisplay::slotPaste(bool bClipboard)
         if (_groupdigits) {
             tmp_str.remove(KGlobal::locale()->thousandsSeparator());
         }
+        tmp_str = tmp_str.toLower();
 
         // determine base
-	if (tmp_str.startsWith("0x", Qt::CaseInsensitive)) {
+	if (tmp_str.startsWith("0x")) {
             tmp_num_base = NB_HEX;
-        } else if (tmp_str.startsWith("0b", Qt::CaseInsensitive)) {
+        } else if (tmp_str.startsWith("0b")) {
             tmp_num_base = NB_BINARY;
             tmp_str.remove(0, 2);
         }
