1ab8cedc46c33e2941947b67acd0d0ed5fd2d36e
Evan Teran <eteran@alum.rit.edu>
2010-09-04 00:12:44 +0000

    returning a const value serves no purpose in practice, it does not protect anything since returning by value in itself is a copy of the value.
    svn path=/trunk/KDE/kdeutils/kcalc/; revision=1171492

--- kcalc_core.h
+++ kcalc_core.h
@@ -163,16 +163,16 @@ private:
 
     KNumber evalOperation(const KNumber &arg1, Operation operation, const KNumber &arg2);
 
-    const KNumber Deg2Rad(const KNumber &x) const {
+    KNumber Deg2Rad(const KNumber &x) const {
         return KNumber(2) * KNumber::Pi() / KNumber(360) * x;
     }
-    const KNumber Gra2Rad(const KNumber &x) const {
+    KNumber Gra2Rad(const KNumber &x) const {
         return KNumber(2)*KNumber::Pi() / KNumber(400) * x;
     }
-    const KNumber Rad2Deg(const KNumber &x) const {
+    KNumber Rad2Deg(const KNumber &x) const {
         return KNumber(360) / (KNumber(2) * KNumber::Pi()) * x;
     }
-    const KNumber Rad2Gra(const KNumber &x) const {
+    KNumber Rad2Gra(const KNumber &x) const {
         return KNumber(400) / (KNumber(2)*KNumber::Pi()) * x;
     }
 
