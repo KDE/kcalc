3162cf093e8a1348f321b9dc05a2afd0959b3d51
Nicolas Fella <nicolas.fella@gmx.de>
2021-02-06 17:03:35 +0100

    Add missing include

--- kcalc.cpp
+++ kcalc.cpp
@@ -27,6 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <clocale>
 
+#include <QActionGroup>
 #include <QApplication>
 #include <QCommandLineParser>
 #include <QCursor>
