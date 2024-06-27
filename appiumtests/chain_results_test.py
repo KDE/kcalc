beb25a615ddbb2b4d5dcbe9cdcc535abe02fef41
Gabriel Barrantes <gabriel.barrantes.dev@outlook.com>
2024-06-27 13:57:42 -0600

    Add first appium test
    Test chain result functionality

---
+++ appiumtests/chain_results_test.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2016 Microsoft Corporation. All rights reserved.
+# SPDX-FileCopyrightText: 2024 Gabriel Barrantes <gabriel.barrantes.dev@outlook.com>
+
+import unittest
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+from appium.options.common.base import AppiumOptions
+import selenium.common.exceptions
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class ChainResultTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        options = AppiumOptions()
+        # The app capability may be a command line or a desktop file id.
+        options.set_capability("app", "org.kde.kcalc.desktop")
+        # Boilerplate, always the same
+        self.driver = webdriver.Remote(command_executor='http://127.0.0.1:4723', options=options)
+        # Set a timeout for waiting to find elements. If elements cannot be found
+        # in time we'll get a test failure. This should be somewhat long so as to
+        # not fall over when the system is under load, but also not too long that
+        # the test takes forever.
+        self.driver.implicitly_wait = 10
+
+    @classmethod
+    def tearDownClass(self):
+        # Make sure to terminate the driver again, lest it dangles.
+        self.driver.quit()
+
+    def setUp(self):
+        self.driver.find_element(by=AppiumBy.NAME, value="AC").click()
+
+    def getresults(self):
+        return self.driver.find_element(by="description", value='Result Display').text
+
+    def assertResult(self, actual, expected):
+        wait = WebDriverWait(self.driver, 20)
+        try:
+            wait.until(lambda x: self.getresults() == expected)
+        except selenium.common.exceptions.TimeoutException:
+            pass
+        self.assertEqual(self.getresults(), expected)
+
+    def test_initialize(self):
+        self.driver.find_element(by=AppiumBy.NAME, value="AC").click()
+
+    def test_chain_binary_operator(self):
+        self.driver.find_element(by=AppiumBy.NAME, value="1").click()
+        self.driver.find_element(by=AppiumBy.NAME, value="+").click()
+        self.driver.find_element(by=AppiumBy.NAME, value="7").click()
+        self.assertResult(self.getresults(), "8")
+        self.driver.find_element(by=AppiumBy.NAME, value="=").click()
+        self.driver.find_element(by=AppiumBy.NAME, value="×").click()
+        self.driver.find_element(by=AppiumBy.NAME, value="2").click()
+        self.assertResult(self.getresults(), "16")
+
+    def test_chain_function(self):
+        self.driver.find_element(by=AppiumBy.NAME, value="8").click()
+        self.driver.find_element(by=AppiumBy.NAME, value="9").click()
+        self.driver.find_element(by=AppiumBy.NAME, value="+").click()
+        self.driver.find_element(by=AppiumBy.NAME, value="1").click()
+        self.assertResult(self.getresults(), "90")
+        self.driver.find_element(by=AppiumBy.NAME, value="=").click()
+        self.driver.find_element(by=AppiumBy.NAME, value="sin").click()
+        self.assertResult(self.getresults(), "1")
+
+if __name__ == '__main__':
+    unittest.main()
