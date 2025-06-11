06bcb29ee000c43967a2880f689c77f0fccdeb42
Viraj Malia <virajmalia@gmail.com>
2025-06-10 22:14:10 -0700

    Apply QT coding style to statusbar and priority levels

--- kcalc_statusbar.cpp
+++ kcalc_statusbar.cpp
@@ -12,20 +12,20 @@
 
 KCalcStatusBar::KCalcStatusBar(QWidget *parent)
     : QStatusBar(parent)
-    , shift_indicator_(
+    , m_shiftIndicator(
           addIndicator(QList<QString>() << i18nc("Normal button functions are active", "NORM") << i18nc("Second button functions are active", "SHIFT")))
-    , base_indicator_(addIndicator(QList<QString>() << i18nc("Decimal indicator in status bar", "DEC") << i18nc("Binary indicator in status bar", "BIN")
+    , m_baseIndicator(addIndicator(QList<QString>() << i18nc("Decimal indicator in status bar", "DEC") << i18nc("Binary indicator in status bar", "BIN")
                                                     << i18nc("Octal indicator in status bar", "OCT") << i18nc("Hexadecimal indicator in status bar", "HEX")))
-    , angle_mode_indicator_(addIndicator(QList<QString>() << i18nc("Degree indicator in status bar", "DEG") << i18nc("Radian indicator in status bar", "RAD")
-                                                          << i18nc("Grad indicator in status bar", "GRA")))
-    , memory_indicator_(addIndicator(QList<QString>() << QString() << i18nc("Memory indicator in status bar", "M")))
+    , m_angleModeIndicator(addIndicator(QList<QString>() << i18nc("Degree indicator in status bar", "DEG") << i18nc("Radian indicator in status bar", "RAD")
+                                                         << i18nc("Grad indicator in status bar", "GRA")))
+    , m_memoryIndicator(addIndicator(QList<QString>() << QString() << i18nc("Memory indicator in status bar", "M")))
 {
     setSizeGripEnabled(false);
 }
 
 QLabel *KCalcStatusBar::addIndicator(const QList<QString> &indicatorTexts)
 {
-    auto l = new QLabel(indicatorTexts.at(0), this);
+    auto *l = new QLabel(indicatorTexts.at(0), this);
 
     // find the widest indicator text
     QFontMetrics fm(l->font());
@@ -45,17 +45,17 @@ QLabel *KCalcStatusBar::addIndicator(const QList<QString> &indicatorTexts)
 
 void KCalcStatusBar::setBaseIndicatorVisible(bool visible)
 {
-    base_indicator_->setVisible(visible);
+    m_baseIndicator->setVisible(visible);
 }
 
 void KCalcStatusBar::setAngleModeIndicatorVisible(bool visible)
 {
-    angle_mode_indicator_->setVisible(visible);
+    m_angleModeIndicator->setVisible(visible);
 }
 
 void KCalcStatusBar::setShiftIndicator(bool shift)
 {
-    shift_indicator_->setText(shift ? i18nc("Second button functions are active", "SHIFT") : i18nc("Normal button functions are active", "NORM"));
+    m_shiftIndicator->setText(shift ? i18nc("Second button functions are active", "SHIFT") : i18nc("Normal button functions are active", "NORM"));
 }
 
 void KCalcStatusBar::setBase(int base)
@@ -77,7 +77,7 @@ void KCalcStatusBar::setBase(int base)
         text = i18n("HEX");
         break;
     }
-    base_indicator_->setText(text);
+    m_baseIndicator->setText(text);
 }
 
 void KCalcStatusBar::setAngleMode(AngleMode mode)
@@ -95,12 +95,12 @@ void KCalcStatusBar::setAngleMode(AngleMode mode)
         text = i18n("GRA");
         break;
     }
-    angle_mode_indicator_->setText(text);
+    m_angleModeIndicator->setText(text);
 }
 
 void KCalcStatusBar::setMemoryIndicator(bool memory)
 {
-    memory_indicator_->setText(memory ? i18nc("Memory indicator in status bar", "M") : QString());
+    m_memoryIndicator->setText(memory ? i18nc("Memory indicator in status bar", "M") : QString());
 }
 
 #include "moc_kcalc_statusbar.cpp"
